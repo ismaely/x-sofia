cc083b9053b3bd7a1dd8fee5d7660f17bd3341ac
ismaely <7ilipe@gmail.com>
2021-10-02 00:17:47 +0100

    files

--- validate/views.py
+++ validate/views.py
@@ -1,3 +1,11 @@
+"""
+ * @author [Gunza Ismael]
+ * @email [7ilip@gmail.com]
+ * @create date 2021-10-01 18:53:46
+ * @modify date 2021-10-01 18:53:46
+ * @desc [description]
+ */
+ """
 from django.shortcuts import render
 from django.db.models import Count, Exists, Q
 from pessoa.models import Pessoa
