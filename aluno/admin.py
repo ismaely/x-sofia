c2a5eedca1fc9214953a5dcd5436bfe1012c2704
ismaely <7ilipe@gmail.com>
2021-10-16 21:09:21 +0100

    chance file picture

--- aluno/admin.py
+++ aluno/admin.py
@@ -3,5 +3,11 @@ from aluno.models import Aluno, Inscricao
 
 # Register your models here.
 
-admin.site.register(Aluno)
-admin.site.register(Inscricao)
+class InscricaoAdmin(admin.ModelAdmin):
+    list_display = ('id','pessoa_id','curso', 'periodo', 'habilitacao_literaria', 'data_inscricao')
+
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('id','pessoa_id', 'instituicao_origem', 'data_registro','media_conclusao')
+
+admin.site.register(Aluno, AlunoAdmin)
+admin.site.register(Inscricao, InscricaoAdmin)
