717b29732b1d9a70280daeb5c6b88fecefb154a0
ismaely <7ilipe@gmail.com>
2021-10-26 11:33:29 +0100

    crate filder for staticfiles

--- Sofia/settings.py
+++ Sofia/settings.py
@@ -148,6 +148,8 @@ else:
 
 SATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+django_heroku.settings(locals())
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
